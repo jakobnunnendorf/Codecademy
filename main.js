f70c9f075e262c7a8c158b9f80ddba39746389ea
jakobnunnendorf <95524074+jakobnunnendorf@users.noreply.github.com>
2022-04-05 21:03:26 +0800

    Step 17

--- main.js
+++ main.js
@@ -63,7 +63,8 @@ nextTwo.onclick = function(){
 
 // Write anonymous event handler property and function for the third progress button
 nextThree.onclick=function(){
-    
+    startOver.hidden=false;
+    nextThree.hidden=true;
 }
 
 // This is the event handler property and function for the startOver button

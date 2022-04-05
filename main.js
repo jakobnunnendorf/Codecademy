4cfef4e9f4bddbe52bfd182fe05adb5b430b9c16
jakobnunnendorf <95524074+jakobnunnendorf@users.noreply.github.com>
2022-04-05 17:09:08 +0800

    Step 1: At the beginning of the code, we have variable name assignments for the keys array and the notes array. There is also a function looping through the elements of the keys array and pushing them to the notes array.  After the second comment, create a function named keyPlay that changes the background color of the keys when they are pressed down.  Be sure to use the .target property in the function because the target is being modified in this case.

--- main.js
+++ main.js
@@ -7,6 +7,9 @@ keys.forEach(function(key){
 
 // Write named functions that change the color of the keys below
 
+const keyPlay=(event)=>{
+event.target.style.backgroundColor='gray';
+}
 
 // Write a named function with event handler properties
 
